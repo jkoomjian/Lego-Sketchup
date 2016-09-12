c4f046e1311cdc118cede0639fcd5252a63c930f
Jonathan <jkoomjian@gmail.com>
2016-09-12 12:30:19 -0700

    spelling

--- lego-space.js
+++ lego-space.js
@@ -4,7 +4,7 @@ class LegoSpace {
     var w = this.elem.offsetWidth;
     var y = this.elem.offsetHeight;
 
-    // Transform styles set here so the can be modified from this.elem.style.transform
+    // Transform styles are set here so they can be modified from this.elem.style.transform
     this.elem.style.transform = "rotateX(-10deg) rotateY(-15deg)";
   }
 }
