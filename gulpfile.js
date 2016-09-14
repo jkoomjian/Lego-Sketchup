c916d7fa90573a7a2fa7edce2b9b4889ea2a5c19
Jonathan <jkoomjian@gmail.com>
2016-09-14 13:41:08 -0700

    reorganizing

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,47 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
+const del = require('del');
+const concat = require('gulp-concat');
+
+gulp.task('clean', () => {
+  del.sync('dist');
+});
 
 gulp.task('sass', () => {
-  gulp.src('style.scss')
+  gulp.src('src/style.scss')
       .pipe(sass())
       .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-      .pipe(gulp.dest('.'));
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', () => {
+  gulp.src(['src/js/main.js', "src/js/lego-space.js", 'src/js/planes.js', 'src/js/lego.js',
+            'src/js/events.js', 'src/js/drag.js', 'src/js/transform.js', 'src/js/start.js'])
+  .pipe(babel( {presets: ['es2015']} ))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('dist'));
+
+  gulp.src(['src/js/main.js', "src/js/lego-space.js", 'src/js/planes.js', 'src/js/lego.js',
+            'src/js/events.js', 'src/js/drag.js', 'src/js/transform.js', 'src/js/start.js'])
+  .pipe(babel( {presets: ['es2015']} ))
+  .pipe(concat('main-test.js'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['*.scss'], ['sass']);
+  gulp.watch(['src/*.scss'], ['sass']);
+  gulp.watch(['src/js/*.js'], ['js']);
+  gulp.watch(['src/*.html'], ['html']);
 });
 
-gulp.task('compilejs', () => {
-  gulp.src(['*.js', '!gulpfile.js'])
-  .pipe(babel( {presets: ['es2015']} ))
-  .pipe(gulp.dest('dist'))
-
-});
-
-gulp.task('default', ['sass'], () => {
+gulp.task('default', ['clean', 'sass', 'js', 'html'], () => {
 });
